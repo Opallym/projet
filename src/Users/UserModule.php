03d8f19458d24042d744223c061b437676b00eeb
Benson1510 <benytelo3@gmail.com>
2025-04-22 15:39:39 +0200

    BENY MIG

--- src/Users/UserModule.php
+++ src/Users/UserModule.php
@@ -4,6 +4,10 @@ namespace App\Users;
 
 class UserModule
 {
+    const DEFINITIONS = __DIR__ . '/config.php';
+    const MIGRATIONS = __DIR__ . '/db/migrations';
+    const SEEDS= __DIR__ . '/db/seeds';
+    
     public function __construct()
     {
         echo 'User';
