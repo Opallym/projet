8fb912831ccb7400bbb3d35f105bd17ccc2f1d7a
Gwendoline53 <Gwendolinecadet73@gmail.com>
2025-04-25 08:54:17 +0200

    Mise a jour gwen

--- src/Cart/db/migrations/20250423120556_create_cart_table.php
+++ src/Cart/db/migrations/20250423120556_create_cart_table.php
@@ -8,13 +8,19 @@ final class CreateCartTable extends AbstractMigration
 {
     public function change(): void
     {
-        $this->table('Cart')
-            ->addColumn('slug','string')
-            ->addColumn('Reference','string')
-            ->addColumn('PriceTTC','decimal')
-            ->addColumn('priceHT','decimal')
-            ->addColumn('EntryDate','datetime')
-            ->addColumn('ReleaseDate','datetime')
-            ->create();
+        $usersTable = $this->table('users');
+        $usersTable->addColumn('name', 'string')
+                  ->addColumn('email', 'string')
+                  ->create();
+
+        // Création de la table Cart ensuite
+        $cartTable = $this->table('Cart');
+        $cartTable->addColumn('slug', 'string')
+                  ->addColumn('Reference', 'string')
+                  ->addColumn('Pricettc', 'decimal')
+                  ->addColumn('priceht', 'decimal')
+                  ->addColumn('entryDate', 'datetime')
+                  ->addColumn('releaseDate', 'datetime')
+                  ->create();
     }
 }
